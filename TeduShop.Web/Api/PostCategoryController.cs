d06def1d837490b5ff7a95025463aa1c0692a828
anh123minh <phamanhminh231@gmail.com>
2018-11-30 21:11:00 +0300

    automapper for web

--- TeduShop.Web/Api/PostCategoryController.cs
+++ TeduShop.Web/Api/PostCategoryController.cs
@@ -2,9 +2,12 @@
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AutoMapper;
 using TeduShop.Model.Models;
 using TeduShop.Service;
 using TeduShop.Web.Infrastructure.Core;
+using TeduShop.Web.Models;
+using TeduShop.Web.Infrastructure.Extensions;
 
 namespace TeduShop.Web.Api
 {
@@ -26,14 +29,16 @@ namespace TeduShop.Web.Api
             {
                 var listCategory = _postCategoryService.GetAll();
 
-                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listCategory);
+                var listPostCategoryVm = Mapper.Map<List<PostCategoryViewModel>>(listCategory);
 
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listPostCategoryVm);
 
                 return response;
             });
         }
 
-        public HttpResponseMessage Post(HttpRequestMessage request, PostCategory postCategory)
+        [Route("add")]
+        public HttpResponseMessage Post(HttpRequestMessage request, PostCategoryViewModel postCategoryVm)
         {
             return CreateHttpResponse(request, () =>
             {
@@ -44,17 +49,20 @@ namespace TeduShop.Web.Api
                 }
                 else
                 {
-                    var category = _postCategoryService.Add(postCategory);
+                    PostCategory newPostCategory = new PostCategory();
+                    newPostCategory.UpdatePostCategory(postCategoryVm);
+
+                    var category = _postCategoryService.Add(newPostCategory);
                     _postCategoryService.Save();
 
                     response = request.CreateResponse(HttpStatusCode.Created, category);
-
                 }
                 return response;
             });
         }
 
-        public HttpResponseMessage Put(HttpRequestMessage request, PostCategory postCategory)
+        [Route("update")]
+        public HttpResponseMessage Put(HttpRequestMessage request, PostCategoryViewModel postCategoryVm)
         {
             return CreateHttpResponse(request, () =>
             {
@@ -65,11 +73,12 @@ namespace TeduShop.Web.Api
                 }
                 else
                 {
-                    _postCategoryService.Update(postCategory);
+                    var postCategoryDb = _postCategoryService.GetById(postCategoryVm.ID);
+                    postCategoryDb.UpdatePostCategory(postCategoryVm);
+                    _postCategoryService.Update(postCategoryDb);
                     _postCategoryService.Save();
 
                     response = request.CreateResponse(HttpStatusCode.OK);
-
                 }
                 return response;
             });
---
+++ TeduShop.Web/Infrastructure/Extensions/EntityExtentions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TeduShop.Model.Models;
+using TeduShop.Web.Models;
+
+namespace TeduShop.Web.Infrastructure.Extensions
+{
+    public static class EntityExtentions
+    {
+        public static void UpdatePostCategory(this PostCategory postCategory, PostCategoryViewModel postCategoryVm)
+        {
+            postCategory.ID = postCategoryVm.ID;
+            postCategory.Alias = postCategoryVm.Alias;
+            postCategory.Name = postCategoryVm.Name;
+            postCategory.Description = postCategoryVm.Description;
+            postCategory.ParentID = postCategoryVm.ParentID;
+            postCategory.DisplayOrder = postCategoryVm.DisplayOrder;
+            postCategory.Image = postCategoryVm.Image;
+            postCategory.HomeFlag = postCategoryVm.HomeFlag;
+
+            postCategory.CreatedDate = postCategoryVm.CreatedDate;
+            postCategory.CreatedBy = postCategoryVm.CreatedBy;
+            postCategory.UpdatedDate = postCategoryVm.UpdatedDate;
+            postCategory.UpdatedBy = postCategoryVm.UpdatedBy;
+            postCategory.MetaKeyword = postCategoryVm.MetaKeyword;
+            postCategory.MetaDescription = postCategoryVm.MetaDescription;
+            postCategory.Status = postCategoryVm.Status;
+        }
+
+        public static void UpdatePost(this Post postCategory, PostViewModel postCategoryVm)
+        {
+            postCategory.ID = postCategoryVm.ID;
+            postCategory.Alias = postCategoryVm.Alias;
+            postCategory.Name = postCategoryVm.Name;
+            postCategory.Description = postCategoryVm.Description;
+            postCategory.CategoryID = postCategoryVm.CategoryID;
+            postCategory.Content = postCategoryVm.Content;
+            postCategory.Image = postCategoryVm.Image;
+            postCategory.HomeFlag = postCategoryVm.HomeFlag;
+            postCategory.HotFlag = postCategoryVm.HotFlag;
+            postCategory.ViewCount = postCategoryVm.ViewCount;
+
+            postCategory.CreatedDate = postCategoryVm.CreatedDate;
+            postCategory.CreatedBy = postCategoryVm.CreatedBy;
+            postCategory.UpdatedDate = postCategoryVm.UpdatedDate;
+            postCategory.UpdatedBy = postCategoryVm.UpdatedBy;
+            postCategory.MetaKeyword = postCategoryVm.MetaKeyword;
+            postCategory.MetaDescription = postCategoryVm.MetaDescription;
+            postCategory.Status = postCategoryVm.Status;
+        }
+    }
+}
