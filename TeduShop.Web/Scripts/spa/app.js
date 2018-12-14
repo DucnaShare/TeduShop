bcb0b1326f9a531bddcdbe3f796026d1efeb8564
anh123minh <phamanhminh231@gmail.com>
2018-12-15 01:27:17 +0300

    cannot resolve tag directive

--- TeduShop.Web/Scripts/spa/app.js
+++ TeduShop.Web/Scripts/spa/app.js
@@ -2,11 +2,41 @@
 
 var myApp = angular.module('myModule', []);
 
-myApp.controller('myController', myController);
+myApp.controller("schoolController", schoolController);
 
-myController.$inject = ['$scope'];
+myApp.directive("teduShopDirective", teduShopDirective);
 
-//declare controller
-function myController($scope) {
-    $scope.message = "This is my message from Controller";
+myApp.service('validatorService', validatorService);
+
+
+
+schoolController.$inject = ['$scope', 'validatorService'];
+
+function schoolController($scope, validatorService) {
+
+    $scope.checkNumber = function () {
+        $scope.message = validatorService.checkNumber($scope.num);
+    }
+    $scope.num = 1;
+}
+
+function validatorService($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return 'This is even';
+        }
+        else
+            return 'This is odd';
+    }
+
+}
+
+function teduShopDirective() {
+    return {
+        restrict: "E",
+        templateUrl: "/Scripts/spa/teduShopDirective.html"
+    }
 }
